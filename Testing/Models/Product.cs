ed063d529391cfb810695fdc2af7ce61de5397d3
Isaiah Wilson <isaiahrwilson@gmail.com>
2023-08-01 11:04:11 -0500

    ASPNET intro exercise

---
+++ Testing/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Testing.Models
+{
+	public class Product
+	{
+		public Product()
+		{
+		}
+
+		public int ProductId { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int CategoryId { get; set; }
+        public bool OnSale { get; set; }
+        public int StockLevel { get; set; }
+
+
+
+    }
+}
+
